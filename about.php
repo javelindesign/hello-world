a5d7f4229454d52e5984ebe38748099eea97b81d
javelindesign <info@javelindesign.com>
2016-09-08 13:08:48 -0400

    Add files via upload

---
+++ about.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<title>Precious Farm</title>
+
+<?php include("includes/includes.php"); ?>
+
+</head>
+
+<body>
+
+<?php include("includes/header.php"); ?>
+
+<div class="container">
+      <div class="row">
+        <div class="col-xs-10 col-xs-offset-1">
+          <h1>About Us</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed metus et ligula vehicula pharetra. Donec quis commodo ipsum. Nam nec libero fringilla velit aliquet commodo at sollicitudin turpis. Suspendisse pulvinar, lectus nec vehicula egestas, dui eros tincidunt mi, quis auctor ex tellus in leo. Aenean lacus arcu, luctus sed aliquet nec, accumsan ac sapien. Morbi a imperdiet leo, quis pharetra nunc. Vestibulum tincidunt rhoncus tempus. Nunc ultrices metus eu ligula sollicitudin, id finibus lacus sodales. Quisque condimentum iaculis magna ut finibus. Phasellus luctus pretium augue, gravida aliquet libero aliquam eget. Nam in vehicula arcu. Duis dignissim interdum velit, quis consequat ante facilisis in.</p>
+          <p>In non sapien eros. Sed non eros vitae justo ullamcorper varius. Cras posuere ipsum tellus, quis rhoncus lacus auctor nec. Vestibulum vel lacus id felis vehicula iaculis. Etiam eget leo tellus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean condimentum dolor at turpis dictum congue. Donec dictum interdum tincidunt.</p>
+          <p>Etiam ultricies, nulla sed rhoncus pulvinar, ligula erat scelerisque neque, in mattis massa turpis non tellus. Praesent sed ipsum molestie, luctus sapien a, scelerisque lorem. Vestibulum dictum, justo sit amet porta facilisis, nunc odio consequat tortor, quis congue enim massa non tortor. Sed at orci nisi. Morbi dui tortor, suscipit eu dui quis, sodales aliquam mauris. In rhoncus id nibh a tincidunt. Nunc molestie iaculis feugiat. Praesent gravida molestie dignissim. Sed laoreet diam ut velit finibus lobortis. Aenean condimentum at neque a tempor. Vivamus est dolor, sollicitudin quis volutpat sit amet, feugiat in tellus. Duis sed laoreet nunc.</p>
+          <p>Vestibulum ante nulla, imperdiet vel enim et, pretium commodo massa. Ut ac mattis metus, non mattis tortor. Donec fermentum, massa sit amet consectetur tempor, elit massa pulvinar lacus, quis rhoncus magna tellus eu est. Vestibulum ac vehicula lacus, vel ultrices massa. Etiam accumsan, ligula eget convallis viverra, ipsum ipsum fermentum diam, fermentum vestibulum tortor massa et arcu. Morbi vestibulum dolor quam, eget venenatis ante posuere id. Donec ut dapibus dolor. Duis nec pharetra tortor. Maecenas vel ex non ex finibus malesuada. Phasellus et porta augue. Duis vehicula, odio tincidunt euismod tincidunt, risus metus porta ante, sit amet tristique magna sem nec diam. Suspendisse potenti. Vivamus convallis porta diam. Morbi maximus nisl sed consectetur vehicula. Ut scelerisque sapien quis rhoncus scelerisque. </p>
+        </div>
+      </div>
+ </div>
+    
+ <?php include("includes/footer.php"); ?>
+  
+</body>
+</html>
